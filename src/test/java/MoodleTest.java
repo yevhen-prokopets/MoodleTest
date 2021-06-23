03aab10484c4c212ef7cc5414e76da240aabbbf4
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-23 16:13:48 +0300

    ssl

--- src/test/java/MoodleTest.java
+++ src/test/java/MoodleTest.java
@@ -1,8 +1,9 @@
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.Keys;
 
 
 import java.util.concurrent.TimeUnit;
@@ -14,6 +15,7 @@ public class MoodleTest {
     @Test
     public void MoodleLogin() {
         WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
         webDriver.get("https://moodle.chnu.edu.ua/login/index.php");
         webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.MINUTES);
         webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/div/div/div/div/div[2]/div[1]/div/a")).click();
@@ -21,7 +23,13 @@ public class MoodleTest {
         webDriver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button")).click();
         webDriver.findElement(By.name("password")).sendKeys("Toradora1");
         webDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"searchBtn\"]")).click();
+        webDriver.findElement(By.className("form-control")).sendKeys("розробка додатків");
+        webDriver.findElement(By.className("form-control")).sendKeys(Keys.ENTER);
+    }
+
+
 
     }
-}
+
 

09e953fb3d2bb1de6c4540184ac3ba85c11dcd0f
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-23 12:15:27 +0300

    ssl

--- src/test/java/MoodleTest.java
+++ src/test/java/MoodleTest.java
@@ -23,28 +23,5 @@ public class MoodleTest {
         webDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button")).click();
 
     }
-
-    @Test
-    public void Git() {
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://github.com/yevhen-prokopets");
-        webDriver.findElement(By.xpath("//*[@id=\"js-contribution-activity\"]/div[2]/div/div[2]/div[2]/details/div/ul/li[2]/span[1]/span/a")).click();
-    }
-    @Test
-    public void GitF() {
-        System.setProperty("webdriver.geckodriver.driver", "D:\\firedriver\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://github.com/yevhen-prokopets");
-        driver.findElement(By.xpath("//*[@id=\"js-contribution-activity\"]/div[2]/div/div[2]/div[2]/details/div/ul/li[2]/span[1]/span/a")).click();
-    }
-    @Test
-    public void authenticateUsing() {
-        WebDriver webDriver = new FirefoxDriver();
-        webDriver.manage().deleteAllCookies();
-        webDriver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        webDriver.get("https://moodle.chnu.edu.ua/login/index.php");
-        webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/div/div/div/div/div[2]/div[1]/div/a")).click();
-
-    }
 }
+

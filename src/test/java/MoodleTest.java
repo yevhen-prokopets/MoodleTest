e95a78665988c5116e8e91d516b214df4531bb58
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-24 08:56:07 +0300

    ssl

--- src/test/java/MoodleTest.java
+++ src/test/java/MoodleTest.java
@@ -23,14 +23,28 @@ public class MoodleTest {
         webDriver.findElement(By.xpath("//*[@id=\"searchBtn\"]")).click();
         webDriver.findElement(By.className("form-control")).sendKeys("розробка додатків");
         webDriver.findElement(By.className("form-control")).sendKeys(Keys.ENTER);
-
-
         }
 
+    @Test
+    public void MoodleTest1() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.get("https://moodle.chnu.edu.ua/login/index.php");
+        webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.MINUTES);
+        webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/div/div/div/div/div[2]/div[1]/div/a")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("prokopets.yevhen@chnu.edu.ua");
+        webDriver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button")).click();
+        webDriver.findElement(By.name("password")).sendKeys("Toradora1");
+        webDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button")).click();
+        webDriver.findElement(By.className("caret")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"action-menu-1-menu\"]/a[2]")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/div/section[4]/div/ul/li[1]/span/a\n")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/a")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"id_subject\"]")).sendKeys("Я був дуже радий тут вчитися");
+        webDriver.findElement(By.xpath("//*[@id=\"id_summary_editoreditable\"]")).sendKeys("Навчання в університеті допомогло мені знайти те чим я хочу займатись");
+        webDriver.findElement(By.xpath("//*[@id=\"id_submitbutton\"]")).click();
     }
-
-
-
+}
 
 
 
